f3e42b9e7050bd4d2945aa5363d717b38dd0fd51
Daniel John Benton <danieljbwork@gmail.com>
2017-09-09 20:07:23 +0100

    east,north were wrong way around, this may require other fixes

--- create_background/create3.js
+++ create_background/create3.js
@@ -4,7 +4,7 @@ let FS = require("fs");
 
 let data = JSON.parse(FS.readFileSync("../data/geonames/GB_IM_combined.json", "utf8"));
 
-let csv = "north,east";
+let csv = "east,north";
 
 for(let i = 0; i < data.length; i++)
 {
